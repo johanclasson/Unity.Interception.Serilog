d7089e3df6a8f751b3128fbe90aab320e957e0f1
Johan Classon <johan@classon.eu>
2016-05-02 13:44:00 +0200

    Log to SQL server

---
+++ src/TS.Framework.Logging.Tests/LogTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Serilog;
+using Xunit;
+
+namespace TS.Framework.Logging.Tests
+{
+    public class LogTests
+    {
+        [Fact]
+        public void ShouldBeAbleToWriteToSqlServer()
+        {
+            ILogger log = new LoggerConfiguration()
+                .WriteTo.MSSqlServer(connectionString: @"Server=.;Database=Serilog;Trusted_Connection=True;", tableName: "Logs")
+                .CreateLogger();
+            var exception = new NotImplementedException("Level 1", new NotImplementedException("Level 2", new NotImplementedException("Level 3")));
+            log.Information(exception, "The current weather is {@Weather}.", new { Condition = "Sunny", WindSpeed = 12.3});
+        }
+    }
+}
