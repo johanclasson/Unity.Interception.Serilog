2199ccea109dbf8fc5307ae1041b2ecef6a30809
Johan Classon <johan@classon.eu>
2016-05-02 14:39:28 +0200

    Elasticsearch

--- src/TS.Framework.Logging.Tests/LogTests.cs
+++ src/TS.Framework.Logging.Tests/LogTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Serilog;
+using Serilog.Sinks.Elasticsearch;
 using Xunit;
 
 namespace TS.Framework.Logging.Tests
@@ -10,10 +11,26 @@ namespace TS.Framework.Logging.Tests
         public void ShouldBeAbleToWriteToSqlServer()
         {
             ILogger log = new LoggerConfiguration()
-                .WriteTo.MSSqlServer(connectionString: @"Server=.;Database=Serilog;Trusted_Connection=True;", tableName: "Logs")
+                .WriteTo.MSSqlServer(connectionString: @"Server=.;Database=Serilog;Trusted_Connection=True;",
+                    tableName: "Logs")
                 .CreateLogger();
-            var exception = new NotImplementedException("Level 1", new NotImplementedException("Level 2", new NotImplementedException("Level 3")));
-            log.Information(exception, "The current weather is {@Weather}.", new { Condition = "Sunny", WindSpeed = 12.3});
+            var exception = new NotImplementedException("Level 1",
+                new NotImplementedException("Level 2", new NotImplementedException("Level 3")));
+            log.Information(exception, "The current weather is {@Weather}.", new {Condition = "Sunny", WindSpeed = 12.3});
+        }
+
+        [Fact]
+        public void ShouldBeAbleToWriteToElasticsearch()
+        {
+            ILogger log = new LoggerConfiguration()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+                {
+                    AutoRegisterTemplate = true
+                })
+                .CreateLogger();
+            var exception = new NotImplementedException("Level 1",
+                new NotImplementedException("Level 2", new NotImplementedException("Level 3")));
+            log.Information(exception, "The current weather is {@Weather}.", new {Condition = "Sunny", WindSpeed = 8.4});
         }
     }
 }
